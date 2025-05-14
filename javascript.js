8b620d96552ff2372524fc74bb8f832d39bba226
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-05-14 16:26:56 +0100

    Wrote the Gamecontroller module

--- javascript.js
+++ javascript.js
@@ -51,3 +51,95 @@ function player(name, symbol) {
         symbol: symbol
     }; 
 }
+
+// Create the module that runs the game:controls turns, checks win/tie, interacts with players
+const GameController = (function () {
+// The first part will be the player instances
+
+  const player1 = player("Player 1", "X");
+  const player2 = player("Player 2", "O");
+  let currentPlayer = player1; 
+
+// An array to store the different winnnig combinations.
+  const winCombos = [
+    [0,1,2], [3,4,5], [6,7,8], // rows
+    [0,3,6], [1,4,7], [2,5,8], // columns
+    [0,4,8], [2,4,6]           // diagonals
+  ];
+
+// A Method that will simply help to switch from one player to another.
+  function switchPlayer() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+// Method which will loop through all the winCombos to check if the currentPlayer has filled all 3 cells in the combo.
+  function checkWin() {
+    const board = Gameboard.getBoard();
+    return winCombos.some(function (combo) {
+      return combo.every(function (i) {
+        return board[i] === currentPlayer.symbol;
+      });
+    });
+  }
+
+// Method which will check if every cell is filled and there is no winner. 
+  function checkTie() {
+    return Gameboard.getBoard().every(function (cell) {
+      return cell !== "";
+    });
+  }
+
+// Game Loop with input
+  function startGame() {
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    function askMove() {
+      Gameboard.printBoard();
+      readline.question(currentPlayer.name + " (" + currentPlayer.symbol + "), choose a cell (1-9): ", function (input) {
+        const index = parseInt(input) - 1;
+
+        if (index >= 0 && index < 9) {
+          if (Gameboard.updateCell(index, currentPlayer.symbol)) {
+            if (checkWin()) {
+              Gameboard.printBoard();
+              console.log(currentPlayer.name + " wins!");
+              readline.close();
+              return;
+            }
+
+            if (checkTie()) {
+              Gameboard.printBoard();
+              console.log("It's a tie!");
+              readline.close();
+              return;
+            }
+
+            switchPlayer();
+            askMove();
+          } else {
+            console.log("Cell already taken. Try again.");
+            askMove();
+          }
+        } else {
+          console.log("Invalid input. Choose a number between 1 and 9.");
+          askMove();
+        }
+      });
+    }
+
+    askMove();
+  }
+
+  return {
+    startGame: startGame
+  };
+})();
+
+
+
+
+
+
